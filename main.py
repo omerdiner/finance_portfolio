57d72cba2a4dd394f7994021c4785458431c0b0d
Omer Diner <omerdiner90@gmail.com>
2023-10-15 22:57:09 +0300

    enough for today

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import api.get_prices as get_prices
+import data.data_manager as data_manager
+
+data_manager.add_new_financial_data()
+
+#akbank = data_manager.get_all_data_for_a_stock("AKBNK")
+#usd = data_manager.get_all_data_for_a_currency("USD")
+#gold = data_manager.get_all_data_for_gold()
