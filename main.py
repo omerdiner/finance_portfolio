466c69732614a2467e0fdff2e0d7aada4dbc8711
Omer Diner <omerdiner90@gmail.com>
2023-10-16 19:32:17 +0300

    making progress

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import api.get_prices as get_prices
 import data.data_manager as data_manager
-
-data_manager.add_new_financial_data()
-
+import data.user_data_manager as user_data_manager
+import tkinter as tk
+import gui.operations as operations
+import gui.currency_operations as currency_operations
 #akbank = data_manager.get_all_data_for_a_stock("AKBNK")
 #usd = data_manager.get_all_data_for_a_currency("USD")
 #gold = data_manager.get_all_data_for_gold()
+
+
+def main():
+    # data_manager.add_new_financial_data()
+
+    currency_chart = currency_operations.CurrencyOperations()
+    currency_chart.show_currency_data()
+
+
+if __name__ == "__main__":
+    main()
